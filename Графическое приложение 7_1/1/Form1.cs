05030acfd0278f738bff1de0a3730418ca65b538
VictoriaKuzminova <119403787+VictoriaKuzminova@users.noreply.github.com>
2022-12-12 21:00:32 +0300

    Add files via upload

---
+++ Графическое приложение 7_1/1/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static string remove(string str)
+        {
+            //Проверка на четность символов
+            if (str.Length % 2 == 0)
+            {
+                //Удаление
+                return str.Remove(str.Length / 2 - 1, 2);
+            }
+            //Если количество символов нечетное, удаление одного символа
+            return str.Remove(str.Length / 2, 1);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            res_textBox.Text = "";
+            string str="";
+            //Проверка на пустоту
+
+            try
+            {
+                str = start_textBox.Text;
+                if (String.IsNullOrWhiteSpace(str))
+                {
+                    MessageBox.Show("Строка пустая");
+                    return;
+                }
+            }
+            catch { }
+            
+           res_textBox.Text+="Количество символов: " + str.Length;
+            res_textBox.Text +="\n"+remove(str);
+        }
+    }
+}
