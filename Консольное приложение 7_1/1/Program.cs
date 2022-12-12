4a6ff8674dc2f735cecbdcf0d11e3a84f753b50c
VictoriaKuzminova <119403787+VictoriaKuzminova@users.noreply.github.com>
2022-12-12 21:01:34 +0300

    Add files via upload

---
+++ Консольное приложение 7_1/1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static string remove(string str)
+        {
+            //Проверка на четность символов
+            if (str.Length % 2 == 0)
+            {
+                //Удаление
+                return str.Remove(str.Length / 2 - 1, 2);
+            }
+            //Если количество символов нечетное, удаление одного символа
+            return str.Remove(str.Length / 2, 1);
+        }
+        static void Main(string[] args)
+        {
+            
+            string str;
+            //Проверка на пустоту
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Введите строку:  ");
+                    str = Console.ReadLine();
+                    if (String.IsNullOrWhiteSpace(str))
+                    {
+                        Console.WriteLine("Строка пустая");
+                        continue;
+                    }
+                    break;
+                }
+                catch { }
+            }
+            Console.WriteLine("Количество символов: "+str.Length);
+            Console.WriteLine("{0}", remove(str));
+            Console.ReadLine();
+        }
+    }
+}
